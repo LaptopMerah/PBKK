1b3a74564e6b2400abc0482df6fca3241657a9db
LaptopMerah” <iqballramadhan18@gmail.com>
2024-09-21 22:21:15 +0700

    feat: Model

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\News;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home']);
@@ -10,58 +13,11 @@ Route::get('/about', function () {
     return view('about', ['title' => 'About']);
 });
 Route::get('/news', function () {
-    return view('news', ['title' => 'News', 'news' => [
-        [
-            'id' => 1,
-            'title' => 'Lorem Ipsum 1',
-            'slug' => 'lorem-ipsum-1',
-            'image' => 'https://flowbite.com/docs/images/blog/image-1.jpg',
-            'content' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis voluptatem magnam quis aut dolores quaerat consequatur tempore provident voluptates commodi, minima quo id blanditiis officia dolore magni eligendi illo? Possimus sint quasi ipsum nihil, deleniti dolorum dolore perferendis eveniet, optio, voluptatum quidem placeat maiores. Voluptas voluptate velit ratione labore nulla.',
-        ],
-        [
-            'id' => 2,
-            'title' => 'Lorem Ipsum 2',
-            'slug' => 'lorem-ipsum-2',
-            'image' => 'https://flowbite.com/docs/images/blog/image-1.jpg',
-            'content' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis voluptatem magnam quis aut dolores quaerat consequatur tempore provident voluptates commodi, minima quo id blanditiis officia dolore magni eligendi illo? Possimus sint quasi ipsum nihil, deleniti dolorum dolore perferendis eveniet, optio, voluptatum quidem placeat maiores. Voluptas voluptate velit ratione labore nulla.',
-        ],
-        [
-            'id' => 3,
-            'title' => 'Lorem Ipsum 3',
-            'slug' => 'lorem-ipsum-3',
-            'image' => 'https://flowbite.com/docs/images/blog/image-1.jpg',
-            'content' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis voluptatem magnam quis aut dolores quaerat consequatur tempore provident voluptates commodi, minima quo id blanditiis officia dolore magni eligendi illo? Possimus sint quasi ipsum nihil, deleniti dolorum dolore perferendis eveniet, optio, voluptatum quidem placeat maiores. Voluptas voluptate velit ratione labore nulla.',
-        ]
-    ]]);
+    return view('news', ['title' => 'News', 'news' => News::all()]);
 });
 
 Route::get('/news/{slug}', function ($slug) {
-    $news = [
-        [
-            'id' => 1,
-            'title' => 'Lorem Ipsum 1',
-            'slug' => 'lorem-ipsum-1',
-            'image' => 'https://flowbite.com/docs/images/blog/image-1.jpg',
-            'content' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis voluptatem magnam quis aut dolores quaerat consequatur tempore provident voluptates commodi, minima quo id blanditiis officia dolore magni eligendi illo? Possimus sint quasi ipsum nihil, deleniti dolorum dolore perferendis eveniet, optio, voluptatum quidem placeat maiores. Voluptas voluptate velit ratione labore nulla.',
-        ],
-        [
-            'id' => 2,
-            'title' => 'Lorem Ipsum 2',
-            'slug' => 'lorem-ipsum-2',
-            'image' => 'https://flowbite.com/docs/images/blog/image-1.jpg',
-            'content' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis voluptatem magnam quis aut dolores quaerat consequatur tempore provident voluptates commodi, minima quo id blanditiis officia dolore magni eligendi illo? Possimus sint quasi ipsum nihil, deleniti dolorum dolore perferendis eveniet, optio, voluptatum quidem placeat maiores. Voluptas voluptate velit ratione labore nulla.',
-        ],
-        [
-            'id' => 3,
-            'title' => 'Lorem Ipsum 3',
-            'slug' => 'lorem-ipsum-3',
-            'image' => 'https://flowbite.com/docs/images/blog/image-1.jpg',
-            'content' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis voluptatem magnam quis aut dolores quaerat consequatur tempore provident voluptates commodi, minima quo id blanditiis officia dolore magni eligendi illo? Possimus sint quasi ipsum nihil, deleniti dolorum dolore perferendis eveniet, optio, voluptatum quidem placeat maiores. Voluptas voluptate velit ratione labore nulla.',
-        ]
-    ];
-
-    //use collect
-    $post = collect($news)->where('slug', $slug)->first();
+    $post = News::find($slug);
 
     return view('news-post', ['title' => 'News Post', 'post' => $post]);
 
