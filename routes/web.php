c856143b111856a4a8e4e2b9ce7a170e6e19be80
LaptopMerah <iqballramadhan18@domain.com>
2024-10-08 17:31:10 +0700

    feat: N + 1 Problem

--- routes/web.php
+++ routes/web.php
@@ -11,24 +11,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home', ['title' => 'Home']);
 });
+
 Route::get('/about', function () {
     return view('about', ['title' => 'About']);
 });
+
 Route::get('/news', function () {
-    return view('news', ['title' => 'News', 'news' => News::all()]);
+    $news = News::with('user', 'category')->latest()->get();
+    return view('news', ['title' => 'News', 'news' => $news]);
 });
 
 Route::get('/news/{news:slug}', function (News $news) {
+    $news->load(['user', 'category']);
     return view('news-post', ['title' => 'News Post', 'post' => $news]);
 });
 
-Route::get('/authors/{user:username}', function (User $user) {
-    return view('news', ['title' => count($user->news) .' News by '. $user->name, 'news' => $user->news]);
+Route::get('/authors/{user:username}', function (User $user) {  
+    $news = $user->news->load('user','category');
+    return view('news', ['title' => count($user->news) .' News by '. $user->name, 'news' => $news]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('news', ['title' => 'News Category in '. $category->type->value, 'news' => $category->news]);
-
+    $news = $category->news->load('user','category');
+    return view('news', ['title' => 'News Category in '. $category->type->value, 'news' => $news]);
 });
 
 Route::get('/contact', function () {
